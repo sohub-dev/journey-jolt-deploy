9269ccd87e095339ea14ad2042e75fba62571bfd
socratesomiliadis <s.omiliadis@gmail.com>
2025-02-23 01:16:30 +0200

    ready for supabase

--- db/index.ts
+++ db/index.ts
@@ -11,7 +11,11 @@ const globalForDb = globalThis as unknown as {
   conn: postgres.Sql | undefined;
 };
 
-const conn = globalForDb.conn ?? postgres(process.env.DATABASE_URL!);
+const conn =
+  globalForDb.conn ??
+  postgres(process.env.DATABASE_URL!, {
+    prepare: false,
+  });
 if (process.env.NODE_ENV !== "production") globalForDb.conn = conn;
 
 export const db = drizzle(conn, { schema });
