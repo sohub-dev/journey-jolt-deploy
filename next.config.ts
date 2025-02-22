de64e126c16be2bc88cc06f08c21036a58d8b031
socratesomiliadis <s.omiliadis@gmail.com>
2025-02-22 23:41:39 +0200

    hmm

--- next.config.ts
+++ next.config.ts
@@ -16,9 +16,9 @@ const nextConfig: NextConfig = {
     ],
   },
   reactStrictMode: false,
-  // env: {
-  //   NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
-  // },
+  env: {
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+  },
 };
 
 export default nextConfig;
